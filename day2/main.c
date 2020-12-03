344d4d2174b057572e95cb682dfd07cb8b7f38e8
Morten Linderud <morten@linderud.pw>
2020-12-03 09:54:50 +0100

    Picked up some improvements
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- day2/main.c
+++ day2/main.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 
-#define for_each_arr(i, items) \
-    for (i = 0; i < sizeof(items) / sizeof(items[0]); i++)
-
 int main(int argc, char *argv[]){
     FILE *f;
     char line[256];
+    int lower, upper;
+    char letter;
+    char password[255];
 
     if (argc == 1){
         printf("No file supplied\n");
@@ -18,11 +18,10 @@ int main(int argc, char *argv[]){
         printf("Can't open file\n");
         return 1;
     }
+
     int valid1 = 0;
+    int valid2 = 0;
     while (fgets(line, sizeof(line), f)) {
-        int lower, upper;
-        char letter;
-        char password[255];
         // 3-4 f: sgbmjfftf
         sscanf(line, "%d-%d %c: %s", &lower, &upper, &letter, password);
         /* printf("lower: %d upper: %d letter: %c password: %s\n", lower, upper, letter, password); */
@@ -36,28 +35,12 @@ int main(int argc, char *argv[]){
         if ((count >= lower) && (count <= upper)){
             valid1++;
         }
-    }
-    printf("Solution 1: %d\n", valid1);
-
-    if (!(f = fopen(argv[1], "re"))){
-        printf("Can't open file\n");
-        return 1;
-    }
-
-    int valid2 = 0;
-    while (fgets(line, sizeof(line), f)) {
-        int lower, upper;
-        char letter;
-        char password[255];
-        sscanf(line, "%d-%d %c: %s", &lower, &upper, &letter, password);
-        /* printf("lower: %d upper: %d letter: %c password: %s\n", lower, upper, letter, password); */
-        if ((password[lower-1] == letter) && (password[upper-1] == letter)){
+        if ((password[lower-1] == letter) ^ (password[upper-1] == letter)){
             continue;
         }
-        if ((password[lower-1] == letter) || (password[upper-1] == letter)){
-            valid2++;
-        }
+        valid2++;
     }
+    printf("Solution 1: %d\n", valid1);
     printf("Solution 2:%d\n", valid2);
     return 0;
 }
