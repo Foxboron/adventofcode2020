427d54cda293b2cdff82590fb77c7cc1b2d170d8
Morten Linderud <morten@linderud.pw>
2020-12-06 18:40:13 +0100

    Solution 2
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- day4/main.c
+++ day4/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <regex.h>
 
 
 typedef struct Passport {
@@ -20,20 +21,82 @@ static const struct Passport EmptyStruct;
 bool check_policy(Passport pass){
     /* printf("check_policy: %s\n", pass.byr); */
     /* printf("check_policy strlen: %i\n", strlen(pass.byr)); */
-    if (strlen(pass.byr) == 0)
+    if (strlen(pass.byr) == 0){
         return false;
-    if (strlen(pass.iyr) == 0)
+    }else{
+        int byr = 0;
+        byr = atoi(pass.byr);
+        if ((byr < 1920) || (byr > 2002)){
+            return false;
+        }
+    }
+    if (strlen(pass.iyr) == 0){
         return false;
-    if (strlen(pass.eyr) == 0)
+    }else{
+        int iyr = 0;
+        iyr = atoi(pass.iyr);
+        if ((iyr < 2010) || (iyr > 2020)){
+            return false;
+        }
+    }
+    if (strlen(pass.eyr) == 0){
         return false;
-    if (strlen(pass.hgt) == 0)
+    }else{
+        int eyr = 0;
+        eyr = atoi(pass.eyr);
+        if ((eyr < 2020) || (eyr > 2030)){
+            return false;
+        }
+    }
+    if (strlen(pass.hgt) == 0){
         return false;
-    if (strlen(pass.hcl) == 0)
+    }else{
+        size_t size = strlen(pass.hgt);
+        char t[3];
+        int val=0;
+        sprintf(t, "%c%c", pass.hgt[size-2], pass.hgt[size-1]);
+        if (!strcmp(t, "cm")){
+            val = atoi(pass.hgt);
+            if ((val < 150) || (val > 193)){
+                return false;
+            }
+        }else if (!strcmp(t, "in")){
+            val = atoi(pass.hgt);
+            if ((val < 59) || (val > 76)){
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+    if (strlen(pass.hcl) == 0){
         return false;
-    if (strlen(pass.ecl) == 0)
+    }else{
+        regex_t regex;
+        int reti=0;
+        reti = regcomp(&regex, "#[0-9a-f]", 0);
+        reti = regexec(&regex, pass.hcl, 0, NULL, 0);
+        if (reti) {
+            return false;
+        }
+    }
+    if (strlen(pass.ecl) == 0){
         return false;
-    if (strlen(pass.pid) == 0)
+    }else{
+        if (!strcmp(pass.ecl, "amb")){
+        }else if (!strcmp(pass.ecl, "blu")){
+        }else if (!strcmp(pass.ecl, "brn")){
+        }else if (!strcmp(pass.ecl, "gry")){
+        }else if (!strcmp(pass.ecl, "grn")){
+        }else if (!strcmp(pass.ecl, "hzl")){
+        }else if (!strcmp(pass.ecl, "oth")){
+        }else{
+            return false;
+        }
+    }
+    if (strlen(pass.pid) != 9){
         return false;
+    }
     return true;
 }
 
